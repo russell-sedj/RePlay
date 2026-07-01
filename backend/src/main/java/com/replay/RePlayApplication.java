7254fa11822a95a2045a3e4a62cc87a3ea1b964c
Russell-Sedj <russelldossouyovo@gmail.com>
2026-07-01 10:13:08 +0200

    chore: initialize spring boot project with dependencies

---
+++ backend/src/main/java/com/replay/RePlayApplication.java
@@ -0,0 +1,12 @@
+package com.replay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RePlayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RePlayApplication.class, args);
+    }
+}
